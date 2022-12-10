5069f33934c0e112434bc5980ca5f02ccc1d1952
244752 <101175640+244752@users.noreply.github.com>
2022-12-10 14:54:44 +0100

    Delete AM_Application-main directory

--- AM_Application-main/README.md
+++
@@ -1,4 +0,0 @@
-# AM_Application
-Application to convert json format texts
-
-![](Image/mainWindow.png)
--- AM_Application-main/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.application.application {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.google.gson;
-
-
-    opens com.application.application to javafx.fxml;
-    exports com.application.application;
-}
